8f2df4116caff40f114815cf3d5ad9302d3cc6d3
renn0xtek9 <renn0xtek9@laposte.net>
2018-11-20 01:17:23 +0100

    Now launching the server in normal or debug mode depending on script argument

--- launch.sh
+++ launch.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-/usr/bin/node ./app.js &
-$($(which google-chrome) "http://127.0.0.1:3001/settings.html")
+if [ "$1" == "Debug" ]
+then 
+        /usr/bin/node --inspect-brk app.js 
+else 
+        /usr/bin/node app.js
+fi
