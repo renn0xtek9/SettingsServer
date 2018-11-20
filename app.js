eaca25f9fc3ff62aa72d3863c68682be8df4e67d
renn0xtek9 <renn0xtek9@laposte.net>
2018-11-20 01:15:24 +0100

    Implement update of settings file

--- app.js
+++ app.js
@@ -52,5 +52,68 @@ server.listen(port, hostname, () => {
 
 function AnalyzePostRequest(body) {
 	var myobj=JSON.parse(body);
-	
+	UpdateSettingsFileWithJSONObject(myobj)
 }
+function ReplaceParameter(newset,oldset)
+{
+	for(var key in newset)
+	{
+		oldset[key]=newset[key];
+	}
+}
+function ParseAndModify(jsonobject,settingsobject)
+{
+	console.log(jsonobject);
+	for (var key in jsonobject)
+	{
+		if(jsonobject.hasOwnProperty(key) && settingsobject.hasOwnProperty(key) && key != 'parameters' )
+		{
+			ParseAndModify(jsonobject[key],settingsobject[key]);			
+		}
+		else{
+			console.log(jsonobject[key]);
+			if (Array.isArray(jsonobject[key])){
+				try{
+					for (var p=0; p<jsonobject[key].length; p++)
+					{
+						var name=jsonobject[key][p]["name"];	//Identify by name 
+						var j=0;
+						while(j<settingsobject[key].length)
+						{
+							if (settingsobject[key][j]["name"]==name)
+							{
+								break;
+							}
+							j+=1;
+						}
+						console.log (jsonobject[key][p]);
+						console.log (settingsobject[key][j]);
+						ReplaceParameter(jsonobject[key][p],settingsobject[key][j]);
+					}
+				}
+				catch (e){
+					console.log("Mismatch while replacing parameter "+e);
+				}
+			}
+			else 
+			{
+	 			settingsobject[key]=jsonobject[key]
+			}
+		}
+	}
+	return;
+}
+function UpdateSettingsFileWithJSONObject(jsonobject)
+{
+	settings = JSON.parse(fs.readFileSync(pathtosettings, 'utf8'));
+	ParseAndModify(jsonobject,settings);
+	console.log("here");
+	console.log(jsonobject);
+	console.log(settings);
+	fs.writeFile('result.json', JSON.stringify(settings,null,2), function(err) {
+		if (err) {
+			console.log(err);
+		}
+	});
+}
+
